0d9d60f30525f2a59b9000fbee06ca9a585556fe
Christian Marquez <christianmarquez41@gmail.com>
2021-03-11 23:10:48 -0500

    added ability to select each conversation

--- client/src/components/Conversations.js
+++ client/src/components/Conversations.js
@@ -4,12 +4,17 @@ import { useConversations } from '../contexts/ConversationsProvider'
 
 export default function Conversations() {
 
-    const { conversations } = useConversations()
+    const { conversations, selectConversationIndex } = useConversations()
 
     return (
         <ListGroup variant="flush">
             {conversations.map((conversation, index) => (
-                <ListGroup.Item key={index}>
+                <ListGroup.Item 
+                key={index}
+                action
+                onClick={() => selectConversationIndex(index)}
+                active={conversation.selected}
+                >
                     {conversation.recipients.map(r => r.name).join(", ")}
                 </ListGroup.Item>
             ))}
--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import useLocalStorage from "../hooks/useLocalStorage"
 import { useContacts } from './ContactsProvider'
 
@@ -11,6 +11,7 @@ export function useConversations() {
 export function ConversationsProvider({ children }) {
 
     const [conversations, setConversations] = useLocalStorage("conversations", [])
+    const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
     const { contacts } = useContacts()
 
     function createConversation(recipients) {
@@ -19,7 +20,7 @@ export function ConversationsProvider({ children }) {
         })
     }
 
-    const formattedConversations = conversations.map(conversation => {
+    const formattedConversations = conversations.map((conversation, index) => {
         const recipients = conversation.recipients.map(recipient => {
             const contact = contacts.find(contact => {
                 return contact.id === recipient
@@ -27,12 +28,14 @@ export function ConversationsProvider({ children }) {
             const name = (contact && contact.name) || recipient
             return { id: recipient, name }
         })
-
-        return { ...conversations, recipients }
+        const selected = index === selectedConversationIndex
+        return { ...conversations, recipients, selected }
     })
 
     const value = {
         conversations: formattedConversations,
+        selectedConversation: formattedConversations[selectedConversationIndex],
+        selectConversationIndex: setSelectedConversationIndex,
         createConversation
     }
 
