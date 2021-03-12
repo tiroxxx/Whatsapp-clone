7babcda306946c7bb9cd12177bf9d8079a0581f7
Christian Marquez <christianmarquez41@gmail.com>
2021-03-11 23:20:26 -0500

    created a component to render selected conversation

---
+++ client/src/components/OpenConversation.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Form, InputGroup, Button } from 'react-bootstrap'
+
+export default function OpenConversation() {
+
+    const [text, setText] = useState("")
+
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
+
+    return (
+        <div className="d-flex flex-column flex-grow-1">
+            <div className="flex-grow-1 overflow-auto">
+
+            </div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="m-2">
+                    <InputGroup>
+                        <Form.Control
+                            as="textarea"
+                            required 
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                            style={{ height: "75px", resize: "none"}}
+                        />
+                        <InputGroup.Append>
+                        <Button type="submit">Send</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Form.Group>
+            </Form>
+        </div>
+    )
+}
