c31ac478b2ba0da30a723b0829fff5eb6cc4e5a8
Christian Marquez <christianmarquez41@gmail.com>
2021-03-11 21:49:14 -0500

    added conversation and contacts component to sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Tab, Nav } from "react-bootstrap"
+import Contacts from './Contacts'
+import Conversations from "./Conversations"
 
 const CONVERSATIONS_KEY = "conversations"
 const CONTACTS_KEY = "contacts"
@@ -10,17 +12,21 @@ export default function Sidebar({ id }) {
 
     return (
         <div style={{ width: "250px" }} className="d-flex flex-column">
-            <Tab.Container activeKey={activeKey}>
+            <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
                 <Nav variant="tabs" className="justify-content-center">
                     <Nav.Item>
-                        <Nav.Link eventKey={CONVERSATIONS_KEY}>
-                            Conversations
-                        </Nav.Link>
-                        <Nav.Link eventKey={CONTACTS_KEY}>
-                            Contacts
-                        </Nav.Link>
+                        <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
+                        <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
                     </Nav.Item>
                 </Nav>
+                <Tab.Content className="border-right overflow-auto flex-grow-1">
+                    <Tab.Pane eventKey={CONVERSATIONS_KEY}>
+                        <Conversations />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey={CONTACTS_KEY}>
+                        <Contacts />
+                    </Tab.Pane>
+                </Tab.Content>
             </Tab.Container>
         </div>
     )
