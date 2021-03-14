08b61061fe83941fe8c2ad9f159a7751629b29d4
Christian Marquez <christianmarquez41@gmail.com>
2021-03-14 16:56:08 -0400

    added sending message to socket

--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import useLocalStorage from "../hooks/useLocalStorage"
 import { useContacts } from './ContactsProvider'
+import { useSocket } from './SocketProvider'
 
 const ConversationsContext = React.createContext()
 
@@ -13,6 +14,7 @@ export function ConversationsProvider({ id, children }) {
     const [conversations, setConversations] = useLocalStorage("conversations", [])
     const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
     const { contacts } = useContacts()
+    const socket = useSocket()
 
     function createConversation(recipients) {
         setConversations(prevConversations => {
@@ -48,6 +50,8 @@ export function ConversationsProvider({ id, children }) {
     }
 
     function sendMessage(recipients, text) {
+        socket.emit("send-message", { recipients, text })
+
         addMessageToConversation({ recipients, text, sender: id })
     }
 
