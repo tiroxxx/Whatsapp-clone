0e19d9e4ab4a9790879f18303bb5c2c7ad03c09c
Christian Marquez <christianmarquez41@gmail.com>
2021-03-14 15:18:40 -0400

    created server with socket.io to start socket between senders and recipients

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { Form, InputGroup, Button } from 'react-bootstrap'
 import { useConversations } from "../contexts/ConversationsProvider"
 
@@ -6,6 +6,11 @@ export default function OpenConversation() {
 
     const [text, setText] = useState("")
     const { sendMessage, selectedConversation } = useConversations()
+    const setRef = useCallback(node => {
+        if (node) {
+            node.scrollIntoView({ smooth: true })
+        }
+    }, [])
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -20,13 +25,15 @@ export default function OpenConversation() {
     return (
         <div className="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
-                <div className="h-100 d-flex flex-column
+                <div className="d-flex flex-column
                 align-items-start justify-content-end px-3">
                     {selectedConversation.messages.map((message, index) => {
+                        const lastMessage = selectedConversation.messages.length - 1 === index
                         return (
                             <div
+                                ref={lastMessage ? setRef : null}
                                 key={index}
-                                className="my-1 d-flex flex-column"
+                                className={`my-1 d-flex flex-column ${message.fromMe ? "align-self-end" : ""}`}
                             >
                                 <div
                                     className={`rounded px-2 py-1 ${message.fromMe ? "bg-primary text-white" : "border"}`}>
